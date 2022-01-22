6ebd57e3407b4505f27cc9f8b50a664a1bd314fe
mia <mia@Mias-MacBook.local>
2022-01-22 16:05:02 +0000

    trying to add a button lol

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -12,6 +12,7 @@ const Tab1: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
+          <IonButton shape="round">Button</IonButton>
           <IonToolbar>
             <IonTitle size="large">Hi! Let's start walking.</IonTitle>
           </IonToolbar>
