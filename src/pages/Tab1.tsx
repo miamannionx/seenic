349f693262663d63151187f38610e0b7dd535f1b
mia <mia@Mias-MacBook.local>
2022-01-22 16:55:17 +0000

    testing buttons again

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import { star } from 'ionicons/icons';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -14,7 +13,7 @@ const Tab1: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Hi! Let's start walking.</IonTitle>
+            <IonTitle size="large">Lets go for a stroll.</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonButton shape ="round" size = "large">
@@ -32,62 +31,5 @@ const Tab1: React.FC = () => {
   );
 };
 
-// import React from 'react';
-
-// import { IonButton, IonIcon, IonContent } from '@ionic/react';
-// import { star } from 'ionicons/icons';
-
-// export const ButtonExample: React.FC = () => (
-//   <IonContent>
-//     {/*-- Default --*/}
-//     <IonButton>Default</IonButton>
-
-//     {/*-- Anchor --*/}
-//     <IonButton href="#">Anchor</IonButton>
-
-//     {/*-- Colors --*/}
-//     <IonButton color="primary">Primary</IonButton>
-//     <IonButton color="secondary">Secondary</IonButton>
-//     <IonButton color="tertiary">Tertiary</IonButton>
-//     <IonButton color="success">Success</IonButton>
-//     <IonButton color="warning">Warning</IonButton>
-//     <IonButton color="danger">Danger</IonButton>
-//     <IonButton color="light">Light</IonButton>
-//     <IonButton color="medium">Medium</IonButton>
-//     <IonButton color="dark">Dark</IonButton>
-
-//     {/*-- Expand --*/}
-//     <IonButton expand="full">Full Button</IonButton>
-//     <IonButton expand="block">Block Button</IonButton>
-
-//     {/*-- Round --*/}
-//     <IonButton shape="round">Round Button</IonButton>
-
-//     {/*-- Fill --*/}
-//     <IonButton expand="full" fill="outline">Outline + Full</IonButton>
-//     <IonButton expand="block" fill="outline">Outline + Block</IonButton>
-//     <IonButton shape="round" fill="outline">Outline + Round</IonButton>
-
-//     {/*-- Icons --*/}
-//     <IonButton>
-//       <IonIcon slot="start" icon={star} />
-//       Left Icon
-//     </IonButton>
-
-//     <IonButton>
-//       Right Icon
-//       <IonIcon slot="end" icon={star} />
-//     </IonButton>
-
-//     <IonButton>
-//       <IonIcon slot="icon-only" icon={star} />
-//     </IonButton>
-
-//     {/*-- Sizes --*/}
-//     <IonButton size="large">Large</IonButton>
-//     <IonButton>Default</IonButton>
-//     <IonButton size="small">Small</IonButton>
-//   </IonContent>
-// );
 
 export default Tab1;
