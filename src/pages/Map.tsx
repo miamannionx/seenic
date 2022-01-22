373bb397e9c272ea091075a1730c43b02cf949d0
mia <mia@Mias-MacBook.local>
2022-01-22 22:16:54 +0000

    fix previous commit's map bug

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,80 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 // import ExploreContainer from '../components/ExploreContainer';
 import './Map.css';
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-const containerStyle = {
-    width: '400px',
-    height: '400px'
+const Map: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Map</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Hi Mia!</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonButton shape ="round" size = "large">
+            Will contain map with top most popular walking routes around current location
+        </IonButton>
+        <br />
+
+      </IonContent>
+    </IonPage>
+  );
 };
-  
-const center = {
-    lat: -3.745,
-    lng: -38.523
-};
-  
-function MyComponent() {
-    const { isLoaded } = useJsApiLoader({
-      id: 'google-map-script',
-      googleMapsApiKey: "AIzaSyDfylAbGmWYsTqo46xDaBnQHgr4dDFwbh0"
-    })
-  
-    const [map, setMap] = React.useState(null)
 
-    const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-    }, [])
-    
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
-
-    return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={map => {
-          const bounds = new window.google.maps.LatLngBounds();
-          map.fitBounds(bounds);
-        }}
-        onUnmount={map => {
-          // do your stuff before map is unmounted
-        }}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    ): <></>
-}
-
-
-export default React.memo(MyComponent);
-
-
-// const Map: React.FC = () => {
-//   return (
-//     <IonPage>
-//       <IonHeader>
-//         <IonToolbar>
-//           <IonTitle>Map</IonTitle>
-//         </IonToolbar>
-//       </IonHeader>
-//       <IonContent fullscreen>
-//         <IonHeader collapse="condense">
-//           <IonToolbar>
-//             <IonTitle size="large">erm</IonTitle>
-//           </IonToolbar>
-//         </IonHeader>
-//         <ExploreContainer name="insert map here" />
-//       </IonContent>
-//     </IonPage>
-
-
-//   );
-// };
-
-// export default Map;
+export default Map;
