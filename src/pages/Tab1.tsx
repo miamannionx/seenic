e0ac3afc092692aae0783c096fc2f894dbdb0043
mia <mia@Mias-MacBook.local>
2022-01-22 16:42:08 +0000

    more buttons and added green

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { star } from 'ionicons/icons';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -12,11 +13,19 @@ const Tab1: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-        <IonButton>Default</IonButton>
           <IonToolbar>
             <IonTitle size="large">Hi! Let's start walking.</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonButton shape ="round" size = "large">
+          Go on walk
+        </IonButton>
+        <IonButton shape ="round" size = "large">
+          View popular walks
+        </IonButton>
+        <IonButton shape ="round" size = "large">
+          View community events
+        </IonButton>
         <ExploreContainer name="Hello." />
       </IonContent>
     </IonPage>
